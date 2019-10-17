283f5fa90fba19b5ae4d76983f9b4ba43f707030
Andrew Larson <andyson456@gmail.com>
2019-10-17 15:49:13 -0700

    ImportantPeople and ImportantLocations models created

---
+++ CommunityInformation/Models/ImportantLocations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityInformation.Models
+{
+	public class ImportantLocations
+	{
+		public string LocationName { get; set; }
+		public string Description { get; set; }
+		public string DateEstablished { get; set; }
+	}
+}
