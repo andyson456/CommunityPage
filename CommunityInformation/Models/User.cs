0113d59e32a384f48a5ec410cc73a30b2e6c5f83
Andrew Larson <andyson456@gmail.com>
2019-11-26 12:14:46 -0800

    Made minor changes to UI

--- CommunityInformation/Models/User.cs
+++ CommunityInformation/Models/User.cs
@@ -7,14 +7,14 @@ namespace CommunityInformation.Models
 {
 	public class User
 	{
-		private List<Comment> comments = new List<Comment>();
+		//private List<Comment> comments = new List<Comment>();
 		//private List<UserMessage> messages = new List<UserMessage>();
 
 		public int UserID { get; set; }
 		public Guid UserKey { get; set; }
 		public string UserName { get; set; }
 
-		public List<Comment> Comments { get { return comments; } }
+		//public List<Comment> Comments { get { return comments; } }
 		//public List<UserMessage> Messages { get { return messages; } }
 	}
 }
