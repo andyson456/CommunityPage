9d2c6e0c4c09debbddca6c366132f43d35661a4a
Andrew Larson <andyson456@gmail.com>
2019-11-26 11:46:06 -0800

    Modified User class

--- CommunityInformation/Models/User.cs
+++ CommunityInformation/Models/User.cs
@@ -8,12 +8,12 @@ namespace CommunityInformation.Models
 	public class User
 	{
 		private List<Comment> comments = new List<Comment>();
-		private List<UserMessage> messages = new List<UserMessage>();
+		//private List<UserMessage> messages = new List<UserMessage>();
 
 		public Guid UserKey { get; set; }
 		public string UserName { get; set; }
 
 		public List<Comment> Comments { get { return comments; } }
-		public List<UserMessage> Messages { get { return messages; } }
+		//public List<UserMessage> Messages { get { return messages; } }
 	}
 }
